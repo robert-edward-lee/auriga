332fe279ef8b96a887a63900e76502927b421ffc
s38449 <r.bolgov@kmz.su>
2024-08-03 20:13:09 +0300

    алгоритм, определяющий что строка содержит только уникальные символы

---
+++ 01_find_unique.c
@@ -0,0 +1,43 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+#define countof(arr) (sizeof(arr) / sizeof(arr[0]))
+
+bool is_unique(const char *s) {
+    static bool hist[256] = { false };
+
+    if(!s) {
+        return false;
+    }
+
+    while(*s) {
+        if(hist[*s]) {
+            return false;
+        }
+        hist[*s++] = true;
+    }
+
+    return true;
+}
+
+struct {
+    const char *str;
+    bool unique;
+} tests[] = {
+    { NULL, false },
+    { "", true },
+    { "abc", true },
+    { "aba", false },
+};
+
+int main(void) {
+    int i;
+
+    for(i = 0; i < countof(tests); ++i) {
+        if(is_unique(tests[i].str) != tests[i].unique) {
+            printf("wrong: is_unique(\"%s\") != %s\n", tests[i].str, tests[i].unique ? "true" : "false");
+        }
+    }
+
+    return 0;
+}
