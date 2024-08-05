9b9282e97287f845088174a2b904f12f5a6be6c4
s38449 <r.bolgov@kmz.su>
2024-08-05 14:45:55 +0300

    функцию которая проверяет корректность произвольной скобочной структуры

--- 06_parens.c
+++ 06_parens.c
@@ -23,6 +23,7 @@ Test tests[] = {
     { "(()()((()))())", true },
     { "{()[]((()))}{}", true },
     { "{()[]((())}{}", false },
+    { "(}", false },
 };
 
 
@@ -54,11 +55,48 @@ bool is_correct_simple(const char *s) {
 }
 
 bool is_correct(const char *s) {
-    return true;
+    int rounds = 0, boxes = 0, curlies = 0;
+
+    if(!s) {
+        return false;
+    }
+
+    while(*s) {
+        switch(*s++) {
+            case '(':
+                ++rounds;
+                break;
+            case ')':
+                --rounds;
+                break;
+            case '[':
+                ++boxes;
+                break;
+            case ']':
+                --boxes;
+                break;
+            case '{':
+                ++curlies;
+                break;
+            case '}':
+                --curlies;
+                break;
+            default:
+                return false;
+        }
+
+        if(rounds < 0 || boxes < 0 || curlies < 0) {
+            return false;
+        }
+    }
+
+    return !rounds && !boxes && !curlies;
 }
 
 int main(void) {
     do_tests(is_correct_simple, tests_simple);
+
+    do_tests(is_correct, tests);
 
     return 0;
 }
